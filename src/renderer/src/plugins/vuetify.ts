c5e2276e60724a61a15ff1937b278373d0540bcd
いのさん <85778832+inoaki1173@users.noreply.github.com>
2026-04-30 11:26:10 +0900

    [add] Vuetify-Themeを作成・適用

---
+++ src/renderer/src/plugins/vuetify.ts
@@ -0,0 +1,27 @@
+import { createVuetify, ThemeDefinition } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+
+const theme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    text: '#000000',
+    textInversion: '#FFFFFF',
+    border: '#808080',
+    btnNumber: '#FFFFFF',
+    btnOperator: '#A7DBF2',
+    btnEqual: '#1984B9',
+    background: '#EBECFA'
+  }
+}
+
+export default createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'theme',
+    themes: { theme }
+  }
+})
