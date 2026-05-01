678bdc1cc2571d7340992ddd33836f3ac28a2219
いのさん <85778832+inoaki1173@users.noreply.github.com>
2026-05-01 15:49:32 +0900

    [add] 電卓コンポーネントを作成(デザインのみ)

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Calculator from '@renderer/components/Calculator.vue'
+</script>
 
 <template>
-  <v-app></v-app>
+  <v-app>
+    <v-main id="main">
+      <Calculator />
+    </v-main>
+  </v-app>
 </template>
