0eb9b28e57cd6da12c4a6a58e39062fdff4386b4
いのさん <85778832+inoaki1173@users.noreply.github.com>
2026-05-11 14:34:40 +0900

    [add] 機能ボタンコンポーネントを作成・適用

--- src/renderer/src/components/NumberPanel.vue
+++ src/renderer/src/components/NumberPanel.vue
@@ -1,11 +1,35 @@
 <script setup lang="ts">
+import { ButtonInfo } from '@renderer/types/buttonType'
+
 import NumberButton from '@renderer/components/NumberButton.vue'
-const numberGrid: string[][] = [
-  ['→', 'CE', 'C'],
-  ['7', '8', '9'],
-  ['4', '5', '6'],
-  ['1', '2', '3'],
-  ['+/-', '0', '.']
+import FunctionButton from '@renderer/components/FunctionButton.vue'
+
+const numberGrid: ButtonInfo[][] = [
+  [
+    { label: '→', type: 'function' },
+    { label: 'CE', type: 'function' },
+    { label: 'C', type: 'function' }
+  ],
+  [
+    { label: '7', type: 'number' },
+    { label: '8', type: 'number' },
+    { label: '9', type: 'number' }
+  ],
+  [
+    { label: '4', type: 'number' },
+    { label: '5', type: 'number' },
+    { label: '6', type: 'number' }
+  ],
+  [
+    { label: '1', type: 'number' },
+    { label: '2', type: 'number' },
+    { label: '3', type: 'number' }
+  ],
+  [
+    { label: '+/-', type: 'function' },
+    { label: '0', type: 'number' },
+    { label: '.', type: 'number' }
+  ]
 ]
 </script>
 
@@ -13,7 +37,8 @@ const numberGrid: string[][] = [
   <div class="number-panel">
     <div v-for="(row, idx) in numberGrid" :key="'row' + (idx + 1)" class="number-row">
       <div v-for="cell in row" :key="'cell' + cell" class="number-cell">
-        <number-button :label="cell" />
+        <number-button v-if="cell.type === 'number'" :label="cell.label" />
+        <function-button v-else-if="cell.type === 'function'" :label="cell.label" />
       </div>
     </div>
   </div>
