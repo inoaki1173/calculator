9b3cd189b40c3591448cad1df31cc5c8857f8d30
いのさん <85778832+inoaki1173@users.noreply.github.com>
2026-05-25 14:32:26 +0900

    [add] 詳細設計を基に型を定義

---
+++ src/renderer/src/types/calculatorType.ts
@@ -0,0 +1,23 @@
+/** 電卓の管理状態 */
+export type CalculatorStateType =
+  | 'IDLE'
+  | 'INPUT_LEFT'
+  | 'INPUT_RIGHT'
+  | 'INPUT_OPERATOR'
+  | 'RESULT'
+
+/** 電卓処理イベント */
+export interface CalculatorEvent {
+  type: CalculatorEventType
+  value?: string
+}
+
+/** 電卓処理イベントの種類 */
+export type CalculatorEventType =
+  | 'DIGIT'
+  | 'OPERATOR'
+  | 'EQUAL'
+  | 'NEGATE'
+  | 'ERASEALL'
+  | 'ERASE'
+  | 'ERASEDIGIT'
