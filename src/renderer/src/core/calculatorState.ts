d638fc585176bb2f94ff3a7fc085cb61f8dcd1f8
いのさん <85778832+inoaki1173@users.noreply.github.com>
2026-06-22 17:01:56 +0900

    [update] 数字入力処理を関数に集約

--- src/renderer/src/core/calculatorState.ts
+++ src/renderer/src/core/calculatorState.ts
@@ -25,9 +25,39 @@ export const MAX_VALUE_SIZE = 10
 export const transition = (state: CalculatorState, event: CalculatorEvent): CalculatorState => {
   const returnedState: CalculatorState = clonedeep(state)
 
-  // 状態とイベントを判定する関数
+  /**
+   * 状態とイベントをそれぞれ比較する
+   * @param status - 電卓の状態
+   * @param type - イベントの種類
+   * @returns - 現在のデータと合致しているか
+   */
   const check = (status: CalculatorStateType, type: CalculatorEventType): boolean => {
     return state.status === status && event.type === type
+  }
+
+  /**
+   * 数字入力処理
+   */
+  const addNumber = (): void => {
+    if (MAX_VALUE_SIZE <= state.currentValue.replace('.', '').length) {
+      return
+    }
+    if (event.value === '.') {
+      // すでに小数点が入力されているなら中止
+      if (state.currentValue.includes('.')) {
+        return
+      }
+      // 初期状態では空白表示のため、0を付け足す
+      if (state.currentValue === '') {
+        returnedState.currentValue = '0'
+      }
+    }
+    // 直前の値が0のみの場合、上書きする
+    else if (state.currentValue === '0') {
+      returnedState.currentValue = ''
+    }
+
+    returnedState.currentValue += event.value
   }
 
   // 状態とイベントに応じて、異なる処理を行う
@@ -35,33 +65,25 @@ export const transition = (state: CalculatorState, event: CalculatorEvent): Calc
     // 初期状態 : 数字入力処理
 
     returnedState.status = 'INPUT_LEFT'
-
-    // 初期状態では何も表示されていないため、0を付け足す
-    if (event.value === '.') {
-      returnedState.currentValue = '0.'
-    }
-    returnedState.currentValue = event.value
+    addNumber()
   } else if (check('INPUT_LEFT', 'DIGIT') || check('INPUT_RIGHT', 'DIGIT')) {
     // 左辺入力状態 / 右辺入力状態 : 数字入力処理
 
-    if (event.value === '.' && state.currentValue.includes('.')) {
-      return state
-    }
-    if (returnedState.currentValue.length < MAX_VALUE_SIZE) {
-      returnedState.currentValue += event.value
-    }
+    addNumber()
   } else if (check('INPUT_OPERATOR', 'DIGIT')) {
     // 演算子入力状態 : 数字入力処理
 
     returnedState.status = 'INPUT_RIGHT'
-    returnedState.currentValue = event.value
+    addNumber()
   } else if (check('RESULT', 'DIGIT')) {
     // 計算結果確定状態 : 数字入力処理
 
     returnedState.status = 'INPUT_LEFT'
-    returnedState.currentValue = event.value
+    returnedState.currentValue = ''
     returnedState.previousValue = ''
     returnedState.operator = ''
+    addNumber()
   }
+
   return returnedState
 }
